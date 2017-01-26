0fbecb36e96c6751e35bc42612bf65c7fd93aa56
nishants12 <sardana.nishant@gmail.com>
2017-01-26 20:13:15 +0530

    wifi_starting

--- app/src/main/java/first/com/hotspot_chat/MainActivity.java
+++ app/src/main/java/first/com/hotspot_chat/MainActivity.java
@@ -1,5 +1,6 @@
 package first.com.hotspot_chat;
 
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,33 +10,37 @@ import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ToggleButton;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    ToggleButton togglehotspot,togglewifi;
+    ToggleButton togglehotspot;
     WifiManager wifi;
     ListView listview;
-    ArrayAdapter adapter;
-
-    StringBuilder sb = new StringBuilder();
+    Button togglewifi;
+    WifiManager wifiManager;
+    WifiReceiver receiverWifi;
+    List<ScanResult> wifiList;
+    List<String> listOfProvider;
+    ListAdapter adapter;
+    ListView listViwProvider;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        togglehotspot= (ToggleButton) findViewById(R.id.toggleButton);
-        listview= (ListView) findViewById(R.id.listView);
-        wifi= (WifiManager) getSystemService(WIFI_SERVICE);
+        togglehotspot = (ToggleButton) findViewById(R.id.toggleButton);
 
-        togglewifi= (ToggleButton) findViewById(R.id.togglewifi);
+        togglewifi = (Button) findViewById(R.id.togglewifi);
 
 
         togglehotspot.setOnClickListener(new View.OnClickListener() {
@@ -46,52 +51,92 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        togglewifi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-                if(isChecked && !wifi.isWifiEnabled()){
-                    wifi.setWifiEnabled(true);
-                }
+        listOfProvider = new ArrayList<String>();
 
-                else if(!isChecked && wifi.isWifiEnabled()){
-                    wifi.setWifiEnabled(false);
-                }
+		/*setting the resources in class*/
+        listViwProvider = (ListView) findViewById(R.id.listView);
 
-            }
-        });
-
-        broadcast reciever=new broadcast();
-        //register reciever
-        registerReceiver(reciever,new IntentFilter(wifi.SCAN_RESULTS_AVAILABLE_ACTION));
-
-
-        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1);
-        listview.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
-
-    }
-
-    class broadcast extends BroadcastReceiver{
-
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-
-            StringBuffer stringBuffer=new StringBuffer();
-            List<ScanResult> list= wifi.getScanResults();
-
-            for(ScanResult scanResult:list){
-                stringBuffer.append((scanResult));
-            adapter.add(scanResult);
-                adapter.notifyDataSetChanged();
-            }
-
-
+        togglewifi.setOnClickListener(this);
+        wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+		/*checking wifi connection
+		 * if wifi is on searching available wifi provider*/
+        if (wifiManager.isWifiEnabled() == true) {
+            togglewifi.setText("ON");
+            scaning();
         }
     }
 
+    private void scaning() {
+        // wifi scaned value broadcast receiver
+        receiverWifi = new WifiReceiver();
+        // Register broadcast receiver
+        // Broacast receiver will automatically call when number of wifi
+        // connections changed
+        registerReceiver(receiverWifi, new IntentFilter(
+                WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        wifiManager.startScan();
+
+    }
+    @Override
+    public void onClick(View arg0) {
+		/* if wifi is ON set it OFF and set button text "OFF" */
+        if (wifiManager.isWifiEnabled() == true) {
+            wifiManager.setWifiEnabled(false);
+            togglewifi.setText("OFF");
+            listViwProvider.setVisibility(ListView.GONE);
+        }
+		/* if wifi is OFF set it ON
+		 * set button text "ON"
+		 * and scan available wifi provider*/
+        else if (wifiManager.isWifiEnabled() == false) {
+            wifiManager.setWifiEnabled(true);
+            togglewifi.setText("ON");
+            listViwProvider.setVisibility(ListView.VISIBLE);
+            scaning();
+        }
+    }
+
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiverWifi);
+    }
+
+    protected void onResume() {
+        registerReceiver(receiverWifi, new IntentFilter(
+                WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        super.onResume();
+    }
+
+    class WifiReceiver extends BroadcastReceiver {
+
+        // This method call when number of wifi connections changed
+        public void onReceive(Context c, Intent intent) {
+            wifiList = wifiManager.getScanResults();
+
+			/* sorting of wifi provider based on level */
+            Collections.sort(wifiList, new Comparator<ScanResult>() {
+                @Override
+                public int compare(ScanResult lhs, ScanResult rhs) {
+                    return (lhs.level > rhs.level ? -1
+                            : (lhs.level == rhs.level ? 0 : 1));
+                }
+            });
+            listOfProvider.clear();
+            String providerName;
+            for (int i = 0; i < wifiList.size(); i++) {
+				/* to get SSID and BSSID of wifi provider*/
+                providerName = (wifiList.get(i).SSID).toString()
+                        + "\n" + (wifiList.get(i).BSSID).toString();
+                listOfProvider.add(providerName);
+            }
+			/*setting list of all wifi provider in a List*/
+            adapter = new ListAdapter(MainActivity.this, listOfProvider);
+            listViwProvider.setAdapter(adapter);
+
+            adapter.notifyDataSetChanged();
+        }
 
 
+    }
 }
-

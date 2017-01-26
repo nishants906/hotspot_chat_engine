4a6cee1dfb64fc9ce2d13c95db90976806fa9f8d
nishants12 <sardana.nishant@gmail.com>
2017-01-26 11:03:36 +0530

    initial

---
+++ app/src/main/java/first/com/hotspot_chat/MainActivity.java
@@ -0,0 +1,19 @@
+package first.com.hotspot_chat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ToggleButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ToggleButton toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
